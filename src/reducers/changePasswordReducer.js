fb6dadd31e3f5ad058b815c4b7a11d474cb79c78
Sanjana H.E <sanjana.he@ahinfotech.in>
2018-11-15 23:55:05 +0530

    added pending flag for changepassword

--- src/reducers/changePasswordReducer.js
+++ src/reducers/changePasswordReducer.js
@@ -1,15 +1,17 @@
 let initialData = []
-const initialState = {"data": initialData, "hasError": false ,"pending" : false}
+const initialState = {"data": initialData, "hasError": null ,"pending" : false}
 
 export default function changePasswordReducer(state = initialState, action) {
     // console.log("action",action)
     switch (action.type) {
         case 'INITIATE_CHANGE_PASSWORD' :
-            return {...state, pending : false}
+            return {...state, hasError: false , pending : true}
         case 'CHANGE_PASSOWORD_SUCCESS': 
-            return { ...state, hasError: false, pending : true }
+            return { ...state, hasError: false, pending : false }
         case 'CHANGE_PASSWORD_FAILURE' :
-            return {...state, message : action.message ,hasError:true, pending : true }
+            return {...state, message : action.message ,hasError:true, pending : false }
+        case 'RESET_CHANGE_PASSWORD' : 
+            return initialState
         default:
             return state
 
