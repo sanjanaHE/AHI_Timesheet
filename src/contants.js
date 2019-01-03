3dca114ed8d149eb269d8fc846336e4c341f5d6c
Abhinaw kumar <abhinaw27392@gmail.com>
2019-01-03 18:46:46 +0530

    changed base url

--- src/contants.js
+++ src/contants.js
@@ -1,3 +1,2 @@
-// export  const baseUrl = "http://localhost:6090/";
-
-export  const baseUrl = "https://my.ahinfotech.in/";
+//export  const baseUrl = "http://localhost:6090/ahits/";
+export  const baseUrl = "https://my.ahinfotech.in/ahits/";
